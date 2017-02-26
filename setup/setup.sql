ab7e699d482e2da2d88f822e8c3452e0dbc6474b
Kevin Eaton <kevinhowardeaton@gmail.com>
2017-02-25 18:21:16 -0600

    Initial commit

---
+++ setup/setup.sql
@@ -0,0 +1,214 @@
+-- MySQL dump 10.13  Distrib 5.6.27, for osx10.10 (x86_64)
+--
+-- Host: localhost    Database: tcgcareer
+-- ------------------------------------------------------
+-- Server version	5.6.27-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Cards`
+--
+
+DROP TABLE IF EXISTS `Cards`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Cards` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cardName` varchar(128) DEFAULT NULL,
+  `cardExpansion` int(11) DEFAULT NULL,
+  `cardOrderValue` int(11) DEFAULT NULL,
+  `cardText` text,
+  PRIMARY KEY (`id`),
+  KEY `cardExpansion` (`cardExpansion`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Cards`
+--
+
+LOCK TABLES `Cards` WRITE;
+/*!40000 ALTER TABLE `Cards` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Cards` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `DeckCards`
+--
+
+DROP TABLE IF EXISTS `DeckCards`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `DeckCards` (
+  `cardId` int(11) NOT NULL,
+  `deckId` int(11) NOT NULL,
+  `cardQuantity` int(11) DEFAULT NULL,
+  UNIQUE KEY `cardId` (`cardId`,`deckId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `DeckCards`
+--
+
+LOCK TABLES `DeckCards` WRITE;
+/*!40000 ALTER TABLE `DeckCards` DISABLE KEYS */;
+/*!40000 ALTER TABLE `DeckCards` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Decks`
+--
+
+DROP TABLE IF EXISTS `Decks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Decks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) NOT NULL,
+  `deckName` varchar(128) DEFAULT NULL,
+  `deckDescription` text,
+  `notes` text,
+  PRIMARY KEY (`id`),
+  KEY `userId` (`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Decks`
+--
+
+LOCK TABLES `Decks` WRITE;
+/*!40000 ALTER TABLE `Decks` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Decks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Expansions`
+--
+
+DROP TABLE IF EXISTS `Expansions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Expansions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `gameId` int(11) NOT NULL,
+  `expansionName` varchar(128) DEFAULT NULL,
+  `releaseYear` int(4) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `gameId` (`gameId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Expansions`
+--
+
+LOCK TABLES `Expansions` WRITE;
+/*!40000 ALTER TABLE `Expansions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Expansions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Games`
+--
+
+DROP TABLE IF EXISTS `Games`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Games` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `game` varchar(128) DEFAULT NULL,
+  `gameType` enum('TCG','Miniatures','Other') DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Games`
+--
+
+LOCK TABLES `Games` WRITE;
+/*!40000 ALTER TABLE `Games` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Games` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Matches`
+--
+
+DROP TABLE IF EXISTS `Matches`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Matches` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) NOT NULL,
+  `opponentId` int(11) DEFAULT '0',
+  `opponentName` varchar(128) DEFAULT NULL,
+  `matchDateTime` datetime DEFAULT NULL,
+  `deckId` int(11) DEFAULT '0',
+  `outcome` enum('Win','Loss','Draw','Incomplete','Practice','Other') DEFAULT NULL,
+  `notes` text,
+  `subWin` int(11) DEFAULT '0',
+  `subLoss` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `userId` (`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Matches`
+--
+
+LOCK TABLES `Matches` WRITE;
+/*!40000 ALTER TABLE `Matches` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Matches` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Users`
+--
+
+DROP TABLE IF EXISTS `Users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(128) DEFAULT NULL,
+  `email` varchar(128) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `signupDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` enum('Verified','Unverified','Disabled') DEFAULT 'Unverified',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Users`
+--
+
+LOCK TABLES `Users` WRITE;
+/*!40000 ALTER TABLE `Users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-02-25 18:14:06
